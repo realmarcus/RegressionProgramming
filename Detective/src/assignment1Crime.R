6165dada50fb2e8b23bf86888563f192e58b5dc5
realmarcus <realmarcus.d@gmail.com>
2017-05-11 19:06:45 +0200

    solve pb 1.3 to 1.6

--- Detective/src/assignment1Crime.R
+++ Detective/src/assignment1Crime.R
@@ -11,3 +11,12 @@ df <- tbl_df(data)
 # How many rows of data (observations) are in this dataset?
 str(df)
 # 191641 obs. of  11 variables
+
+# Using the "max" function, what is the maximum value of the variable "ID"?
+max(df$ID)
+
+# What is the minimum value of the variable "Beat"?
+min(df$Beat)
+
+# How many observations have a LocationDescription value of ALLEY?
+summary(df)
